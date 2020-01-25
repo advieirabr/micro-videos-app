390cdfd259ca15e894794901f4bcffd28aafb81d
Alisson Vieira <advieirabr@gmail.com>
2020-01-25 10:14:59 -0300

    criação dos endpoints referentes a category e genre

---
+++ app/Models/Category.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Category extends Model
+{
+    use SoftDeletes, Traits\Uuid;
+    protected  $dates = ['deleted_at'];
+    protected $fillable = ['name', 'description', 'is_active'];
+
+    protected $casts = [
+      'id' => 'string'
+    ];
+
+    public $incrementing = false;
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['namespace'=> 'Api'], function(){
+
+    $exceptCreateAndEdit = [
+        'except' => ['create', 'edit']
+    ];
+
+    Route::resource('categories', 'CategoryController', $exceptCreateAndEdit);
+    Route::resource('genres', 'GenreController', $exceptCreateAndEdit);
+});
+
