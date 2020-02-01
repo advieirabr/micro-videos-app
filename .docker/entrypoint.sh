faca26ac7130acf30ad9f12c0049bfd76c8e377f
Alisson Vieira <advieirabr@gmail.com>
2020-02-01 12:59:17 -0300

    Finalizando módulo testes com HTTP

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
+cp .env.example .env
+cp .env.testing.example .env.testing
 composer install
+php artisan route:clear
+php artisan config:clear
+php artisan cache:clear
 php artisan key:generate
 php artisan migrate
+
 
 php-fpm
