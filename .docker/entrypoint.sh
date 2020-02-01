47c5393075095be9e315a90ae07f34619227fd08
Alisson Vieira <advieirabr@gmail.com>
2020-02-01 17:12:42 -0300

    Finalizando módulo testes com HTTP

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -4,12 +4,8 @@
 cp .env.example .env
 cp .env.testing.example .env.testing
 composer install
-php artisan route:clear
-php artisan config:clear
-php artisan cache:clear
 php artisan key:generate
 php artisan migrate
-
 
 
 php-fpm
