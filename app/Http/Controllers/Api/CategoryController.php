64a87822a80861cdd0350154ec118bda521710ed
Alisson Vieira <advieirabr@gmail.com>
2020-02-13 23:34:59 -0300

    Video Resource

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -30,34 +30,4 @@ class CategoryController extends BasicCrudController
 
     }
 
-    /*public function index()
-    {
-        return Category::all();
-    }
-
-    public function store(Request $request)
-    {
-        $this->validate($request, $this->rules);
-        $category = Category::create($request->all());
-        $category->refresh();
-        return $category;
-    }
-
-    public function show(Category $category)
-    {
-        return $category;
-    }
-
-    public function update(Request $request, Category $category)
-    {
-        $this->validate($request, $this->rules);
-        $category->update($request->all());
-        return $category;
-    }
-
-    public function destroy(Category $category)
-    {
-        $category->delete();
-        return response()->noContent();
-    }*/
 }
